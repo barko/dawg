7045b28d2dc8706f9c8190060f7e5b65baae7aa0
barko <mika@illouz.net>
2014-02-28 00:37:28 -0800

    documentation and 'stylistic cleansing'

--- huf_hist.ml
+++ huf_hist.ml
@@ -1,6 +1,10 @@
-type bin = { left: float; mean: float; right : float }
+type bin = {
+  left: float;
+  mean: float;
+  right : float
+}
 
-let rec huf_loop best best_f prefix tail = match tail with
+let rec huf_loop best best_f prefix = function
   | [] | [_] ->
     let (best_prefix, best_node, best_tail) = best in
     List.rev_append best_prefix (best_node :: best_tail)
@@ -24,26 +28,28 @@ let rec huf_loop best best_f prefix tail = match tail with
       let candidate = (prefix, candidate_node, nxt_tl) in
       huf_loop candidate candidate_f (hd :: prefix) tl
 
-let huf_one b_plus_one = match b_plus_one with
-  | hd :: ((nxt :: nxt_tl) as tl) ->
-    let (hd_bin, hd_f) = hd in
-    let (nxt_bin, nxt_f) = nxt in
-    let candidate_f = hd_f + nxt_f in
-    let total_f = float (hd_f + nxt_f) in
-    let mean =
-      (hd_bin.mean *. float hd_f +. nxt_bin.mean *. float nxt_f) /. total_f
-    in
-    let candidate_bin = {
-      left = hd_bin.left;
-      mean;
-      right = nxt_bin.right;
-    } in
-    let candidate_node = candidate_bin, candidate_f in
-    let candidate = ([], candidate_node, nxt_tl) in
-    huf_loop candidate candidate_f [hd] tl
-  | _ -> b_plus_one
+let huf_one b_plus_one =
+  match b_plus_one with
+    | hd :: ((nxt :: nxt_tl) as tl) ->
+      let (hd_bin, hd_f) = hd in
+      let (nxt_bin, nxt_f) = nxt in
+      let candidate_f = hd_f + nxt_f in
+      let total_f = float (hd_f + nxt_f) in
+      let mean =
+        (hd_bin.mean *. float hd_f +. nxt_bin.mean *. float nxt_f) /. total_f
+      in
+      let candidate_bin = {
+        left = hd_bin.left;
+        mean;
+        right = nxt_bin.right;
+      } in
+      let candidate_node = candidate_bin, candidate_f in
+      let candidate = ([], candidate_node, nxt_tl) in
+      huf_loop candidate candidate_f [hd] tl
+    | _ -> b_plus_one
 
-let singleton x = { left = x; mean = x; right = x }
+let singleton x =
+  { left = x; mean = x; right = x }
 
 let list_split_n n l =
   let rec iter n prefix tail = match tail with
--- huf_hist.mli
+++ huf_hist.mli
@@ -1,3 +1,18 @@
-type bin = { left: float; mean: float; right : float }
+type bin = {
+  left: float;
+  mean: float;
+  right : float
+}
 
 val create : (float * int) list -> int -> (bin * int) list
+(* [create distinct_values_and_counts num_bins] returns a histogram,
+   represented by a list of bins, and their corresponding count.  Each
+   bin is bounded on by [bin.left] [bin.right], with mean
+   [bin.mean]. The count associated with a bin represent the total sum
+   of the counts of values between the left and right bounds.  The
+   interpretation of [bin.left] and [bin.right] depends on the order
+   of [distinct_values_and_counts]: When [distinct_values_and_counts]
+   is ascending order, [bin.left] represents the upper bound of the
+   bin, and [bin.right] represents the lower bound.  In descending
+   order, the reverse is true.
+*)
