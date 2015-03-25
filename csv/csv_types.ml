900a1f60808931e6fd5a362ecfa8efcad5cdbd44
Alex Baretta <alexbaretta@gmail.com>
2015-03-25 14:38:34 -0700

    [sparse csv] allow empty fields in CSV `dense' rows, interpret them as the anonymous value

--- csv/csv_types.ml
+++ csv/csv_types.ml
@@ -2,3 +2,24 @@ type value = [ `Int of int | `Float of float | `String of string ]
 type dense = value list
 type sparse = (int * value) list
 type row = [ `Dense of dense | `Sparse of sparse | `EOF ]
+type opt_row = value option list
+
+let parse_opt_row_sparse opt_row =
+  let rec loop i accu opt_row =
+    match opt_row with
+    | None :: tl -> loop (succ i) accu tl
+    | (Some v) :: tl -> loop (succ i) ((i,v) :: accu) tl
+    | [] -> accu
+  in
+  loop 0 [] opt_row
+
+exception Sparse
+let unopt = function
+  | Some x -> x
+  | None -> raise Sparse
+
+let parse_opt_row_dense opt_row = List.map unopt opt_row
+
+let parse_opt_row opt_row =
+  try `Dense(parse_opt_row_dense opt_row)
+  with Sparse -> `Sparse(parse_opt_row_sparse opt_row)
